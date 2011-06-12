1f5e3d7a05aa66154f8952b34c4aaa489065d79f
Route56_cp <SND\Route56_cp>
2011-06-12 07:14:51 +0000

    some old changes

---
+++ Gyrfalcon/ClientSideIntegrationTest/DSDCPMSA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesktopClient;
+
+namespace ClientSideIntegrationTest
+{
+	[TestClass]
+	public class DSDCPMSA
+	{
+		// DesktopClient
+		// DataSender
+		// SystemtrayApp
+		// ProcessMonitor
+
+		[TestMethod]
+		public void GetSomeDataOnAppLaunchForFakeProcess()
+		{
+			SystemTrayInterface sysTray = new SystemTrayInterface();
+			ProcessMonitorLauncher launcher = new FakeProcessMonitorLauncher();
+
+			launcher.Start();
+
+			//TODOH sysTray.ProcessMonitor 
+		}
+	}
+}
