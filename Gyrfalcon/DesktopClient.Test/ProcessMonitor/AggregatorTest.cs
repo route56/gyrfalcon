0f8389bac155c3debc67928a86b3f0f0216a95f2
Route56_cp <SND\Route56_cp>
2012-03-10 20:46:00 +0000

    Aggregator logic. Counts sequential same elements. Compresses amount of information stored.

---
+++ Gyrfalcon/DesktopClient.Test/ProcessMonitor/AggregatorTest.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesktopClient.ProcessMonitor;
+
+namespace DesktopClient.Test.ProcessMonitor
+{
+	[TestClass]
+	public class AggregatorTest
+	{
+		[TestMethod]
+		public void GetAggregationResult_NormalInput_ProperCount()
+		{
+			string[,] input = { 
+					{ "Hello", "World" }, 
+					{ "Hello", "World" }, 
+					{ "Hello", "World" }, 
+					{ "Hello1", "World" }, 
+					{ "Hello1", "World" }, 
+					{ "Hello1", "World1" }, 
+					{ "Hello1", "World" }, 
+					{ "Hello1", "World" }, 
+					{ "Hello", "World" }
+							  };
+
+			int[,] expected = { 
+					{ 0, 3 }, 
+					{ 3, 2}, 
+					{ 5, 1}, 
+					{ 6, 2}, 
+					{ 8, 1}
+							  };
+
+			TestTarget(input, expected);
+		}
+
+		[TestMethod]
+		public void GetAggregationResult_OneInput_ProperCount()
+		{
+			string[,] input = { { "Hello", "World" } };
+
+			int[,] expected = { { 0, 1 } };
+
+			TestTarget(input, expected);
+		}
+
+		[TestMethod]
+		public void GetAggregationResult_TwoInput_ProperCount()
+		{
+			string[,] input = { { "Hello", "World" }, { "Hello", "World" } };
+
+			int[,] expected = { { 0, 2 } };
+
+			TestTarget(input, expected);
+		}
+
+		[TestMethod]
+		public void GetAggregationResult_TwoDifferentInput_ProperCount()
+		{
+			string[,] input = { { "Hello", "World" }, { "Hello1", "World" } };
+
+			int[,] expected = { { 0, 1 }, {1, 1} };
+
+			TestTarget(input, expected);
+		}
+
+		[TestMethod]
+		public void GetAggregationResult_ZeroInput_ProperCount()
+		{
+			string[,] input = new string[0, 2];
+
+			int[,] expected = { { 0, 0 } };
+
+			TestTarget(input, expected);
+		}
+
+		private void TestTarget(string[,] input, int[,] expected)
+		{
+			Aggregator target = new Aggregator();
+
+			for (int i = 0; i < input.GetLength(0); i++)
+			{
+				target.Add(input[i, 0], input[i, 1]);
+			}
+
+			int[,] actual = target.GetAggregationResult();
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void GetAggregationResult_RealSample_ProperCount()
+		{
+			string[,] input = {
+{"EXCEL",	"Microsoft Excel - FeatureAreas.xlsx"},
+{"EXCEL",	"Microsoft Excel - FeatureAreas.xlsx"},
+{"EXCEL",	"Microsoft Excel - FeatureAreas.xlsx"},
+{"EXCEL",	"Microsoft Excel - FeatureAreas.xlsx"},
+{"EXCEL",	"Microsoft Excel - FeatureAreas.xlsx"},
+{"EXCEL",	"Microsoft Excel - FeatureAreas.xlsx"},
+{"EXCEL",	"Microsoft Excel - FeatureAreas.xlsx"},
+{"EXCEL",	"Microsoft Excel - FeatureAreas.xlsx"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"Idle",	""},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	""},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	""},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	""},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"},
+{"devenv",	"Gyrfalcon - Microsoft Visual Studio"}
+};
+
+			int[,] expected = 
+			{
+{0,	8},
+{8,	15},
+{23,	1},
+{24,	1},
+{25,	1},
+{26,	7},
+{33,	1},
+{34,	1},
+{35,	1},
+{36,	2}
+			};
+
+			TestTarget(input, expected);
+		}
+
+		[TestMethod]
+		public void GetAggregationResult_CalledMultipleTimes_ProperCountInEnd()
+		{
+			string[,] input = { 
+					{ "Hello", "World" }, 
+					{ "Hello", "World" }, 
+					{ "Hello", "World" }, 
+					{ "Hello1", "World" }, 
+					{ "Hello1", "World" }, 
+					{ "Hello1", "World1" }, 
+					{ "Hello1", "World" }, 
+					{ "Hello1", "World" }, 
+					{ "Hello", "World" }
+							  };
+
+			int[,] expected = { 
+					{ 0, 3 }, 
+					{ 3, 2}, 
+					{ 5, 1}, 
+					{ 6, 2}, 
+					{ 8, 1}
+							  };
+
+			Aggregator target = new Aggregator();
+
+			for (int i = 0; i < input.GetLength(0); i++)
+			{
+				target.Add(input[i, 0], input[i, 1]);
+
+				target.GetAggregationResult();
+			}
+
+			int[,] actual = target.GetAggregationResult();
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ Gyrfalcon/DesktopClient/ProcessMonitor/Aggregator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesktopClient.ProcessMonitor
+{
+	public class Aggregator
+	{
+		private List<Tuple<int, int>> aggregation = new List<Tuple<int, int>>();
+
+		private int start = 0;
+		private int count = 0;
+		private string lastItemA;
+		private string lastItemB;
+
+		public void Add(string itemA, string itemB)
+		{
+			if (count == 0)
+			{
+				lastItemA = itemA;
+				lastItemB = itemB;
+				count = 1;
+			}
+			else
+			{
+				if (lastItemA == itemA && lastItemB == itemB)
+				{
+					count++;
+				}
+				else
+				{
+					aggregation.Add(new Tuple<int, int>(start, count));
+
+					start = start + count;
+					lastItemA = itemA;
+					lastItemB = itemB;
+					count = 1;
+				}
+			}
+		}
+
+		public int[,] GetAggregationResult()
+		{
+			aggregation.Add(new Tuple<int, int>(start, count));
+
+			int[,] result = new int[aggregation.Count, 2];
+
+			for (int i = 0; i < aggregation.Count; i++)
+			{
+				result[i, 0] = aggregation[i].Item1;
+				result[i, 1] = aggregation[i].Item2;
+			}
+
+			aggregation.RemoveAt(aggregation.Count - 1);
+
+			return result;
+		}
+	}
+}
